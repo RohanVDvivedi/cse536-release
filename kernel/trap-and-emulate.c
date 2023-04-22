26f3ffdd164f9e150f7f2a2dbdcd619cf45f8b14
RohanVDvivedi <rohandvivedi@gmail.com>
2023-04-22 14:52:22 -0700

    added utility functions to get information about the register form its code

--- kernel/trap-and-emulate.c
+++ kernel/trap-and-emulate.c
@@ -6,10 +6,24 @@
 #include "proc.h"
 #include "defs.h"
 
+
+int is_readonly(int code)
+{
+    return (((code >> 10) & 0b11) == 0b11);
+}
+
+#define U_MODE_REG 0b00
+#define S_MODE_REG 0b01
+#define M_MODE_REG 0b11
+
+int get_mode(int code)
+{
+    return ((code >> 8) & 0b11);
+}
+
 // Struct to keep VM registers (Sample; feel free to change.)
 struct vm_reg {
-    int     code;
-    int     mode;
+    int     code; // the register's code is enough to get the information about (readable+writable OR readonly) and about (mode in which it is accessible)
     uint64  val;
 };
 
@@ -41,7 +55,7 @@ void trap_and_emulate(void) {
     // faulting instruction
     uint32 instr = *((uint32*)(phy_addr_instr));
 
-    printf("va = %p, pa = %p, instruction = %x\n", virt_addr_instr, phy_addr_instr, instr);
+    printf("va = %p, pa = %p, instruction = %p\n", virt_addr_instr, phy_addr_instr, (uint64)instr);
 
     // all system instructions are I-type instructions
     uint32 op       = ((instr >>  0) & ((1 <<  7) - 1));
@@ -50,6 +64,7 @@ void trap_and_emulate(void) {
     uint32 rs1      = ((instr >> 15) & ((1 <<  5) - 1));
     uint32 upper    = ((instr >> 20) & ((1 << 12) - 1));
 
+    printf("funct3 = %x\n", funct3);
     printf("[PI] op = %x, rd = %x, rs1 = %x, upper = %x\n", op, rd, rs1, upper);
 }
 
